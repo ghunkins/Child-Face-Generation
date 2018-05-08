a9b8ec063c91448051cfd4bcc5f4d7601622c6d7
Greg <ghunkins@users.noreply.github.com>
2018-05-08 15:43:37 -0400

    400 epochs

--- src/model/job.sh
+++ src/model/job.sh
@@ -8,4 +8,4 @@
 #SBATCH --gres=gpu:2
 
 source activate new_pix
-python main.py --backend tensorflow --save_dir /scratch/ghunkins/CUPIDV_RESULTS/ --dset cupidV --nb_epoch 200 --generator deconv --img_dim 128 32 32
+python main.py --backend tensorflow --save_dir /scratch/ghunkins/CUPIDV_RESULTS/ --dset cupidV --nb_epoch 400 --generator deconv --img_dim 128 32 32
