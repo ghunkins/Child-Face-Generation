fcbb76f424739f60aa66f06440ba79e79180f4c7
Greg <ghunkins@users.noreply.github.com>
2018-05-13 11:51:11 -0400

    upsample job

---
+++ src/model/job_upsample.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH -t 0-08:00:00
+#SBATCH --mem 10GB
+#SBATCH --job-name=PIX
+#SBATCH --output=output_PIX_%j.txt
+#SBATCH -e error_PIX_%j.txt
+#SBATCH --gres=gpu:2
+
+source activate new_pix
+python main.py --backend tensorflow --save_dir /scratch/ghunkins/CUPIDV_RESULTS/ --dset cupidV --nb_epoch 400 --img_dim 128 32 32
